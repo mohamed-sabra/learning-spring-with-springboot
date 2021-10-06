8a8de08bf85cf88383b49020d177422591fff723
Mohamed Sabra <mohamad.sabra@isae.edu.lb>
2021-10-06 10:35:43 +0300

    Spring demo project

---
+++ learning-spring/learning-spring/src/test/java/com/mohamed/project313/learningspring/LearningSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohamed.project313.learningspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
